eb4961b300092671b46271c9dfcb59147d33a0e3
Enda Kenneally2 <endakenneally2@gmail.com>
2018-11-05 17:28:23 +0000

    Initial Commit

---
+++ point2d.java
@@ -0,0 +1,23 @@
+public class Point2D {
+	 public Point2D(double x, double y) {
+	// TODO
+	 }
+	 public double x() {
+	 // TODO
+	 }
+	 public double y() {
+	// TODO
+	 }
+	 public double distanceTo(Point2D that) {
+	// TODO
+	 }
+	 public String toString() {
+	// TODO
+	 }
+	 public static void main(String[] args) {
+	 int x0 = 1;
+	 int y0 = 2;
+	 Point2D p = new Point2D(x0, y0);
+	 System.out.println(p);
+	 }
+}
